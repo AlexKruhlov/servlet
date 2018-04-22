006e24af2de0a70e58c4e2f62a4092721dcf0578
Aleksandr Kruglov <alkruglov777@gmail.com>
2018-04-22 19:08:15 +0300

    Added servlet

---
+++ src/main/java/com/example/servletparams/Params.java
@@ -0,0 +1,21 @@
+package com.example.servletparams;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Params extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter printWriter = resp.getWriter();
+		printWriter.println("<html> <body>" + getServletConfig().getInitParameter("First name") + "<p>"
+				+ getServletConfig().getInitParameter("Second name") + "</body></html>");
+	}
+
+}
