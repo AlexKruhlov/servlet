3d9e326ebcd630cd2e091922a4c62478ba7833a5
Aleksandr Kruglov <alkruglov777@gmail.com>
2018-04-22 21:49:41 +0300

    Added context parameter feature

---
+++ src/main/java/com/example/contextinit/Params.java
@@ -0,0 +1,19 @@
+package com.example.contextinit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Params extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter printWriter = resp.getWriter();
+		printWriter.println("<html> <body>" + getServletContext().getInitParameter("message") + "</body></html>");
+	}
+}
